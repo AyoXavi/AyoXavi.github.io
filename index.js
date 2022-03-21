786fe80033064f2d8d51cc10d7dcec38cc77b769
AyoXavi <superboycop@gmail.com>
2022-03-21 00:31:39 -0400

    Added function declarations

---
+++ index.js
@@ -0,0 +1,24 @@
+// Task 2
+function Register() {
+
+}
+
+// Task 4
+function PlayGame() {
+
+}
+
+// Task 6
+function CheckGuess() {
+
+}
+
+// Task 10
+function findPercentageScore() {
+
+}
+
+// Task 13
+function showAll() {
+
+}
